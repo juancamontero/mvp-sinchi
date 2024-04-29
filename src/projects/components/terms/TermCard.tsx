80bda0276a71745389236a35409d8f87909717ad
Juancamontero <juancamilomonteroosorio@192.168.1.104>
2024-04-29 11:03:46 -0500

    db update, terms card updated

--- src/projects/components/terms/TermCard.tsx
+++ src/projects/components/terms/TermCard.tsx
@@ -22,14 +22,14 @@ export const TermCard = ({
   return (
     <Link
       href={`${baseUrl}/${id}`}
-      className='h-48 space-y-3 bg-bg-100 p-2 rounded-sm shadow-md flex flex-row justify-start items-center hover:bg-bg-200'
+      className='h-48 space-y-3 bg-bg-200 p-2 rounded-sm shadow-md flex flex-row justify-start items-center hover:bg-bg-300'
       title={`Proyectos de ${title}`}
     >
-      <div className='w-2/6 h-hull mx-auto text-primary-300 flex items-center justify-center p-2'>
+      <div className='w-1/6 h-hull mx-auto text-primary-300 flex items-center justify-center p-2'>
         {icon}
       </div>
       <div className='flex flex-col justify-normal items-start w-2/3 text-left pl-1'>
-        <h3 className='text-accent-200 text-6xl font-bold'>{projectsCount}</h3>
+        <h3 className='text-accent-200 text-5xl font-bold'>{projectsCount}</h3>
         <h4 className='text-lg text-gray-800 font-semibold'>{title}</h4>
         <h4 className='text-xs text-gray-700 font-semibold'>
           {subTitle ?? ''}
