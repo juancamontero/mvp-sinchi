99a9469f92181a19e58a27cf062998c2736529dc
Juancamontero <juancamilomonteroosorio@192.168.1.104>
2024-06-25 09:05:44 -0500

    to test body css changes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,18 +10,18 @@ export const Footer = () => {
 
   return (
     <>
-      <section className='w-full mt-1 relative px-4 flex items-center justify-center space-x-4  lg:h-20 h-28 bg-bg-300'>
-        <div className='flex flex-row gap-2 justify-center items-stretch w-full lg:py-2'>
-          <div className='w-1/6 '>
+      <section className='w-full relative flex items-center justify-center space-x-4  lg:h-20 h-fit bg-bg-100 pt-2'>
+        <div className='flex flex-row gap-2 justify-center items-stretch w-full  bg-bg-300 px-4 py-2'>
+          <div className='w-10 pr-2 lg:w-auto border-r border-primary-100'>
             <Image
               src={'/logo_nav.png'}
               alt={'Logo SINCHI'}
               height={80}
               width={70}
-              className='object-contain sm:object-scale-down  lg:h-20 lg:w-auto'
+              className='object-contain sm:object-scale-down  lg:h-20 lg:w-auto my-auto' 
             />
           </div>
-          <p className=' w-5/6 text-[10px] sm:text-xs text-text-200 leading-tight text-center max-w-xl border-l border-text-100 pl-2 my-auto'>
+          <p className='h-full w-5/6 text-[10px] sm:text-xs text-text-100 leading-tight text-center  my-auto '>
             Desarrollo: Juan Camilo Montero | Diseño: Paola Aponte | Ilustración
             e iconografía: Gilberto Aponte | Compilación contenido: Ana María
             Franco y Paola Aponte. | Oficina de comunicaciones y Subdirección
